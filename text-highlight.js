d4e939ce8b8714a82ed2fcbf00737ca261dff26f
dodo糯 <effy.yin@gmail.com>
2015-05-05 14:50:31 +0800

    update

--- text-highlight.js
+++ text-highlight.js
@@ -1,42 +1,44 @@
-(function($) {
+(function ($) {
 
 	'use strict';
 
-	var anchorNode, focusNode;
+	var anchorNode, 
+		focusNode;
 
 	function TextHighlight() {
-		var sel = window.getSelection(),			
-			anchorOffset = sel.anchorOffset,
-			focusOffset = sel.focusOffset;
+		var sel = window.getSelection(),
 			anchorNode = sel.anchorNode;
-			focusNode = sel.focusNode;
+			focusNode = sel.focusNode;		
+			anchorOffset = sel.anchorOffset,
+			focusOffset = sel.focusOffset;			
 
 		// anchorNode 和 focusNode 属于同一个文本节点
-		if(anchorNode === focusNode) {
-
-	        var tc = anchorNode.textContent;
-	        var cs = tc.substring(0, anchorOffset);
-	        var cm = tc.substring(anchorOffset, focusOffset);
-	        var ce = tc.substring(focusOffset);
-	        var newTc = cs+'<span style="background:yellow">'+ cm+'</span>'+ce;
+		if (anchorNode === focusNode) {
+	        var tc = anchorNode.textContent,
+	        	cs = tc.substring(0, anchorOffset),
+	        	cm = tc.substring(anchorOffset, focusOffset),
+	        	ce = tc.substring(focusOffset);,
+	        	newTc = cs+'<span style="background:yellow">'+ cm+'</span>'+ce;
 	        $(anchorNode).replaceWith(newTc);	//////////////////	      
 	    } 
 	    // 跨标签高亮
 	    else {
 	    	
 	    	// 分别获取 anchorNode 和 focusNode 祖先节点
-			var anchorAncestors = findAncestorNodes(anchorNode);
-		    var focusAncestors = findAncestorNodes(focusNode);
-        
+			var anchorAncestors = findAncestorNodes(anchorNode),
+				focusAncestors = findAncestorNodes(focusNode);
 
-	        // 找到 anchorNode 和 focusNode 的第一个公共祖先节点
-	        var indexA,indexF,isFind = false;
-	        for(var i=0;i<anchorAncestors.length;i++) {
-	            if(isFind) break;
-	            for(var j=0;j<focusAncestors.length;j++) {
-	                if(anchorAncestors[i]==focusAncestors[j]) {                    
-	                    indexA=i;
-	                    indexF=j;
+	        	// 找到 anchorNode 和 focusNode 的第一个公共祖先节点
+	        	indexA,
+	        	indexF,
+	        	isFind = false;
+
+	        for (var i = 0; i < anchorAncestors.length; i++) {
+	            if (isFind) break;
+	            for (var j = 0; j < focusAncestors.length; j++) {
+	                if (anchorAncestors[i] == focusAncestors[j]) {                    
+	                    indexA = i;
+	                    indexF = j;
 	                    isFind = true;
 	                    break;
 	                }
@@ -44,18 +46,18 @@
 	        }
 
 	        // 对 anchorNode 所属子树以其祖先节点为分界对右侧节点进行高亮
-	        for(var i=0;i<indexA-1;i++) {
+	        for (var i = 0; i < indexA-1; i++) {
 	            highlightRightSiblings(anchorAncestors[i])
 	        }
 
 	        // 对 focusNode 所属子树以其祖先节点为分界对左侧节点进行高亮
-	        for(var j=0;j<indexF-1;j++) {
+	        for (var j = 0; j < indexF-1; j++) {
 	            highlightLeftSiblings(focusAncestors[j])
 	        }
 	        
 	        // 对以公共祖先节点为根节点的中间子树进行高亮
 	        var midRoot = anchorAncestors[indexA-1].nextSibling;
-	        while(midRoot != null && midRoot != focusAncestors[indexF-1]) {
+	        while (midRoot != null && midRoot != focusAncestors[indexF-1]) {
 	            //对midRoot进行高亮时会改变其DOM结构，所以需在高亮前对其nextSibling节点进行缓存
 	            var tmp = midRoot.nextSibling;
 	            highLightNode(midRoot);
@@ -63,17 +65,17 @@
 	        }
 
 	        // 对 anchorNode 进行高亮
-	        var tcA = anchorNode.textContent;  
-	        var csA = tcA.substring(0,anchorOffset);
-	        var ceA = tcA.substring(anchorOffset);
-	        var newTcA = csA+'<span style="background:yellow">'+ ceA+'</span>';
+	        var tcA = anchorNode.textContent,
+	        	csA = tcA.substring(0,anchorOffset),
+	        	ceA = tcA.substring(anchorOffset),
+	        	newTcA = csA + '<span style="background:yellow">' + ceA + '</span>';
 	        $(anchorNode).replaceWith(newTcA);
 
 	        // 对 focusNode 进行高亮
-	        var tcF = focusNode.textContent;
-	        var csF = tcF.substring(0,focusOffset);
-	        var ceF = tcF.substring(focusOffset);
-	        var newTcF = '<span style="background:yellow">'+ csF+'</span>'+ceF;   
+	        var tcF = focusNode.textContent,
+	        	csF = tcF.substring(0,focusOffset),
+	        	ceF = tcF.substring(focusOffset),
+	        	newTcF = '<span style="background:yellow">' + csF + '</span>' + ceF;   
 	        $(focusNode).replaceWith(newTcF);
 	        
 	        // 跨标签高亮时需要手动取消焦点。。。为什么啊啊啊啊 
@@ -100,7 +102,7 @@
 	// 将一个节点及其所有右侧兄弟节点进行高亮
 	function highlightRightSiblings(node) {
 	    highLightNode(node); 
-	    if(!node.nextSibling) {
+	    if (!node.nextSibling) {
 	        return;
 	    } else {
 	        highlightRightSiblings(node.nextSibling);
@@ -110,7 +112,7 @@
 	// 将一个节点及其所有左侧兄弟节点进行高亮
 	function highlightLeftSiblings(node) {
 	    highLightNode(node);    
-	    if(!node.previousSibling) {
+	    if (!node.previousSibling) {
 	        return;
 	    } else {
 	        highlightLeftSiblings(node.previousSibling);
@@ -119,7 +121,7 @@
 
 	// 高亮节点的递归实现，如果是文本节点则直接高亮，如果是元素节点则将其所有文本子节点进行高亮
 	function highLightNode(node) {
-	    if(node.nodeType == 3 && 
+	    if (node.nodeType == 3 && 
 	        /[^\n\r\s]/.test(node.nodeValue) &&	// 排除所有只包含回车、换行或空格的文本呢节点
 	        node != anchorNode &&
 	        node != focusNode) {
@@ -127,10 +129,10 @@
 	        span.style.background = 'yellow';
 	        $(node).wrap($(span));
 	    }
-	    if(node.nodeType == 1) {
+	    if (node.nodeType == 1) {
 	        var childNodes = node.childNodes;
-	        if(childNodes.length>0) {
-	            for(var i=0;i<childNodes.length;i++) {
+	        if (childNodes.length>0) {
+	            for (var i = 0; i < childNodes.length; i++) {
 	                highLightNode(childNodes[i])
 	            }
 	        }
